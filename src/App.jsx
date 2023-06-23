6c0080d1570b28f0671c59085c1ba945c0ed482b
Hayden James Cooper <haydencpr95@gmail.com>
2023-06-23 18:15:33 -0400

    Kinda Stuck

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
 import Results from './Components/Results';
 import './App.scss';
 
+const initialState = {
+  data: null,
+  requestParams: {},
+  loading: false,
+  apiHistory: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_DATA':
+      return { ...state, data: action.payload };
+    case 'SET_REQUEST_PARAMS':
+      return { ...state, requestParams: action.payload };
+    case 'SET_LOADING':
+      return { ...state, loading: action.payload };
+    case 'ADD_API_HISTORY':
+      return { ...state, apiHistory: [...state.apiHistory, action.payload] };
+    default:
+      return state;
+  }
+};
+
 const App = () => {
-  const [data, setData] = useState(null);
-  const [requestParams, setRequestParams] = useState({});
-  const [loading, setLoading] = useState(false);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const callApi = async (requestParams) => {
     try {
-      setLoading(true);
+      dispatch({ type: 'SET_LOADING', payload: true });
+
       const requestOptions = {
         method: requestParams.method,
       };
@@ -29,28 +50,49 @@ const App = () => {
       }
 
       const responseData = await response.json();
-      setData(responseData);
-      setLoading(false);
+
+      dispatch({ type: 'SET_DATA', payload: responseData });
+      dispatch({ type: 'SET_LOADING', payload: false });
+
+      const apiCall = {
+        url: requestParams.url,
+        method: requestParams.method,
+        headers: response.headers,
+        result: responseData,
+      };
+      dispatch({ type: 'ADD_API_HISTORY', payload: apiCall });
     } catch (error) {
       console.error(error);
-      setData(null);
-      setLoading(false);
+      dispatch({ type: 'SET_DATA', payload: null });
+      dispatch({ type: 'SET_LOADING', payload: false });
     }
   };
 
   useEffect(() => {
-    if (Object.keys(requestParams).length !== 0) {
-      callApi(requestParams);
+    if (Object.keys(state.requestParams).length !== 0) {
+      callApi(state.requestParams);
     }
-  }, [requestParams]);
+  }, [state.requestParams]);
 
   return (
     <>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
-      <Form handleApiCall={setRequestParams} />
-      <Results data={data} loading={loading} />
+      <div>Request Method: {state.requestParams.method}</div>
+      <div>URL: {state.requestParams.url}</div>
+      <Form handleApiCall={(requestData) => dispatch({ type: 'SET_REQUEST_PARAMS', payload: requestData })} />
+      <Results data={state.data} loading={state.loading} />
+      <div>
+        <h2>Previous API Calls</h2>
+        {state.apiHistory.map((apiCall, index) => (
+          <div key={index}>
+            <h3>API Call {index + 1}</h3>
+            <div>URL: {apiCall.url}</div>
+            <div>Method: {apiCall.method}</div>
+            <div>Headers: {JSON.stringify(apiCall.headers, null, 2)}</div>
+            <div>Result: {JSON.stringify(apiCall.result, null, 2)}</div>
+          </div>
+        ))}
+      </div>
       <Footer />
     </>
   );
